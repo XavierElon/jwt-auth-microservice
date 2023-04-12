19f9126eb1788ce76e831ad50e2b4e801f2540cf
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-12 15:22:28 -0400

    validate token

--- jwt.ts
+++ jwt.ts
@@ -2,6 +2,21 @@ import { sign, verify } from 'jsonwebtoken'
 
 export const createTokens = (user) => {
     const accessToken = sign({ username: user.username, id: user.id}, 'jwtsecret', { expiresIn: '1h'})
-    console.log(accessToken)
     return accessToken
+}
+
+export const validateToken = (req, res, next) => {
+    const accessToken = req.cookies['access-token']
+
+    if (!accessToken) return res.status(400).json({ error: 'User not authenticated' })
+
+    try {
+        const validToken = verify(accessToken, 'jwtsecret')
+        if (validToken) {
+            req.authenticated = true
+            return next()
+        }
+    } catch(err) {
+        return res.status(400).json({ error: err })
+    }
 }
